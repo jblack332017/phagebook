4d7e11473484c018b58f1363c6ddd6c4f6e71cd6
Josh Black <josh.m.black.work@gmail.com>
2017-12-02 13:29:48 -0700

    added genome search and fetch (#9)

---
+++ fasta/getGenome.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+import sys
+import os
+import shutil
+from Bio import Entrez
+Entrez.email = sys.argv[2] #this parameter is the user's email address
+
+if os.path.exists("genomes"):
+    shutil.rmtree("genomes")
+os.makedirs("genomes")
+
+for accession_no in open(sys.argv[1],"r"): #this parameter is a file with accession numbers
+    accession_no = accession_no.strip()
+    handle = Entrez.efetch(db="nucleotide", id=accession_no, rettype="fasta")
+    file = open("genomes/"+accession_no+".fasta","w+")
+    file.write(handle.read())
+    file.close()
+#each fasta file will be a separate file - named after accession number
