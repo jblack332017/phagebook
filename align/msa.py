550bcc0c60ded4431c10d6fe5c4d85e80d5d1532
Spencer-Smith <spencer.smith2012@gmail.com>
2017-12-07 10:25:01 -0700

    Spencer (#11)
    * Added script for multiple sequence alignment via ClustalW
    
    * added genome search and fetch
    
    * adding click command line interface
    
    * Groundwork for master controller script PhageBook
    
    * Beginning to add functionality to master controller script.
    
    * wip

--- align/msa.py
+++ align/msa.py
@@ -3,11 +3,9 @@ import os
 from Bio.Align.Applications import ClustalwCommandline
 from Bio import AlignIO
 
-if len(sys.argv) < 2:
-  print "msa.py usage: python msa.py [infile]"
-  sys.exit(1)
-if not os.path.exists(sys.argv[1]):
-  print "msa.py: Could not find file: " + sys.argv[1]
-  sys.exit(1)
-cline = ClustalwCommandline("./clustalw2", infile=sys.argv[1])
-stdout, stderr = cline()
+def align(fasta_path, path_to_clustal):
+  if not os.path.exists(fasta_path):
+    print "msa.py: Could not find file: " + fasta_path
+    sys.exit(1)
+  cline = ClustalwCommandline(path_to_clustal, infile=fasta_path)
+  stdout, stderr = cline()
