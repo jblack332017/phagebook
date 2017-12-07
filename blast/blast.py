b84203b00e2da9bc2d78eea59a5a0f81e4721e44
Josh Black <josh.m.black.work@gmail.com>
2017-12-07 11:17:01 -0700

    Reworking blast (#13)
    * added genome search and fetch
    
    * should not add venv
    
    * added correct blast parsing

--- blast/blast.py
+++ blast/blast.py
@@ -3,15 +3,20 @@ from Bio.Blast import NCBIXML
 import sys
 
 
-for index in xrange(1,len(sys.argv)):
-    # Read fasta
-    fasta_file = open(sys.argv[index], 'r').read()
-    # Run blast over the internet
-    result_handle = NCBIWWW.qblast("blastp", "nr", fasta_file)
+# Read fasta
+fasta_file = open(sys.argv[1], 'r').read()
+# Run blast over the internet
+result_handle = NCBIWWW.qblast("blastp", "nr", fasta_file)
+w = open('sequenceIds.txt', 'w')
+# Begin Parsing
+blast_records = NCBIXML.parse(result_handle)
+for blast_record in blast_records:
+    for alignment in blast_record.alignments:
+        for hsp in alignment.hsps:
+            if hsp.expect < sys.argv[2]:
+                idNumbers = alignment.title.split("|")
+                w.write(idNumbers[3]+"\n")
 
-    # Begin Parsing
-    blast_records = NCBIXML.parse(result_handle)
-    print blast_records
 
 #
 #
