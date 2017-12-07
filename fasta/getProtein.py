2c28fff2cb1c5aefd35539bbc38f6bf17ca80034
Josh Black <josh.m.black.work@gmail.com>
2017-12-07 12:11:41 -0700

    adding git ignore

--- fasta/getProtein.py
+++ fasta/getProtein.py
@@ -1,18 +1,18 @@
 #! /usr/bin/env python
-import sys
 import os
 import shutil
 from Bio import Entrez
-Entrez.email = sys.argv[2] #this parameter is the user's email address
 
-if os.path.exists("proteins"):
-    shutil.rmtree("proteins")
-os.makedirs("proteins")
+def getProteins(input_file, email):
+    Entrez.email = email #this parameter is the user's email address
+    if os.path.exists("proteins"):
+        shutil.rmtree("proteins")
+    os.makedirs("proteins")
 
-for accession_no in open(sys.argv[1],"r"): #this parameter is a file with accession numbers
-    accession_no = accession_no.strip()
-    handle = Entrez.efetch(db="protein", id=accession_no, rettype="fasta")
-    file = open("proteins/"+accession_no+".fasta","w+")
-    file.write(handle.read())
-    file.close()
-#each fasta file will be a separate file - named after accession number
+    for accession_no in open(input_file,"r"): #this parameter is a file with accession number
+        accession_no = accession_no.strip()
+        handle = Entrez.efetch(db="protein", id=accession_no, rettype="fasta")
+        file = open("proteins/"+accession_no+".fasta","w+")
+        file.write(handle.read())
+        file.close()
+    #each fasta file will be a separate file - named after accession number
