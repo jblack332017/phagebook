07805d237ad2bf5810b617f0617cbe792293eb02
stewartofgondor <stewartofgondor@gmail.com>
2017-12-09 12:19:00 -0700

    Stewartofgondor patch 6 (#20)
    * Update phagebook.py
    
    * Update runGepard.py
    
    added abspath to gepard script

--- gepard/runGepard.py
+++ gepard/runGepard.py
@@ -2,8 +2,8 @@
 import shlex, subprocess
 import sys
 
-def runGepard(seq1file,seq2file):
-	command_line = "java -cp Gepard-1.40.jar org.gepard.client.cmdline.CommandLine -seq1 "+ seq1file +" -seq2 "+ seq1file +" -matrix matrices/edna.mat -outfile plot.png"
+def runGepard(abs_path,seq1file,seq2file):
+	command_line = "java -cp "+abs_path"+Gepard-1.40.jar org.gepard.client.cmdline.CommandLine -seq1 "+ seq1file +" -seq2 "+ seq1file +" -matrix matrices/edna.mat -outfile plot.png"
 	args = shlex.split(command_line)
 	print args
 	p = subprocess.Popen(args)
