5945ae2dac4a5412ea80a3accf38b377c5afc198
Spencer-Smith <spencer.smith2012@gmail.com>
2017-12-09 13:31:03 -0700

    Added clustal support for nonlinux platforms (#23)

--- align/msa.py
+++ align/msa.py
@@ -1,6 +1,15 @@
 from Bio.Align.Applications import ClustalwCommandline
 from Bio import AlignIO
 
-def align(fasta_path, path_to_clustal):
-  cline = ClustalwCommandline(path_to_clustal, infile=fasta_path)
+def align(fasta_path, path_to_clustal, system, outformat="clustal"):
+  if system == "linux" or system == "linux2":
+    path_to_clustal += ".lin"
+  elif system == "darwin":
+    path_to_clustal += ".mac"
+  elif system == "win32":
+    path_to_clustal += ".win"
+  else:
+    click.echo("Operating system not supported.")
+    return
+  cline = ClustalwCommandline(path_to_clustal, infile=fasta_path, output=outformat)
   stdout, stderr = cline()
