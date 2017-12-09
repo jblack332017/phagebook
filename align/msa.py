166b02aee28683e071ed75eaccf47b86e6ae20ae
Spencer-Smith <spencer.smith2012@gmail.com>
2017-12-09 11:40:24 -0700

    Added relative paths and alignment funciton to phagebook script (#19)

--- align/msa.py
+++ align/msa.py
@@ -1,12 +1,6 @@
-import sys
-import os
-import click
 from Bio.Align.Applications import ClustalwCommandline
 from Bio import AlignIO
 
 def align(fasta_path, path_to_clustal):
-  if not os.path.exists(fasta_path):
-    click.echo("msa.py: Could not find file: " + fasta_path)
-    sys.exit(1)
   cline = ClustalwCommandline(path_to_clustal, infile=fasta_path)
   stdout, stderr = cline()
