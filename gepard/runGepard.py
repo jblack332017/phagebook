165ee42df5f63011f61e79326510a69f33f28151
stewartofgondor <stewartofgondor@gmail.com>
2017-12-14 09:35:01 -0700

    Update runGepard.py

--- gepard/runGepard.py
+++ gepard/runGepard.py
@@ -4,6 +4,5 @@ import os
 import sys
 
 def runGepard(abs_path,seq1file,seq2file):
-	command_line = "java -cp "+abs_path+"Gepard-1.40.jar org.gepard.client.cmdline.CommandLine -seq1 "+ seq1file +" -seq2 "+ seq1file +" -matrix " + abs_path +  "matrices/edna.mat -outfile plot.png"
-	print(command_line)
+	command_line = "java -cp "+abs_path+"Gepard-1.40.jar org.gepard.client.cmdline.CommandLine -seq1 "+ seq1file +" -seq2 "+ seq1file +" -matrix " + abs_path +  "matrices/edna.mat -outfile plot.png -upper 45 -lower 10"
 	subprocess.call(command_line, shell=True)
