ac58360c03d3850aada9a0809c44d34e0c27a709
stewartofgondor <stewartofgondor@gmail.com>
2017-12-13 22:18:54 -0700

    Update runGepard.py

--- gepard/runGepard.py
+++ gepard/runGepard.py
@@ -1,13 +1,9 @@
 #! /usr/bin/env python
-import shlex, subprocess
+import subprocess
+import os
 import sys
 
 def runGepard(abs_path,seq1file,seq2file):
-	command_line = "java -cp "+abs_path+"Gepard-1.40.jar org.gepard.client.cmdline.CommandLine -seq1 "+ seq1file +" -seq2 "+ seq1file +" -matrix " + abs_path +  "matrices/edna.mat -outfile plot.png"
-	args = shlex.split(command_line)
-	print(args)
-	p = subprocess.Popen(args)
-	p.wait()
-
-#runGepard(sys.argv[1],sys.argv[2])
-#can run gepard from command line using 2 files names as arguments
+	command_line = "java -cp "+abs_path+"Gepard-1.40.jar org.gepard.client.cmdline.CommandLine -seq1 "+ seq1file +" -seq2 "+ seq1file +" -matrix " + abs_path +  "matrices/edna.mat -outfile plot.png -upper 45 -lower 10"
+	print(command_line)
+	subprocess.call(command_line, shell=True)
